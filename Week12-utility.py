02cc565030c466713f32c768f25f39d5eef20948
Jakob Schmitz <jkschmitz@mines.edu>
2019-11-19 11:41:48 -0700

    Second def and file to test functionality

--- Week12-utility.py
+++ Week12-utility.py
@@ -6,3 +6,18 @@
 def PrintOutput(string):
     print("OUTPUT " + str(string))
 
+
+def LoadFile(name):
+    ret = []
+    ret2 = []
+    file = open(str(name), "r")
+    ret += [file.readlines()]
+    for i in ret:
+        x = 0
+        for f in i:
+            ret2.append(f.rstrip())
+            x += 1
+    return ret2
+
+
+PrintOutput(LoadFile("test.txt"))
